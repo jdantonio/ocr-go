3c344beee0585c858ce96b8c4c5df6c68313a469
Jerry D'Antonio <stumpjumper@gmail.com>
2015-10-23 14:07:58 -0400

    Reading data file

--- ocr.go
+++ ocr.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+
+	lines := 0
+
+	file, err := os.Open("data.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines += 1
+		fmt.Println(scanner.Text())
+	}
+
+	if lines%4 != 0 {
+		fmt.Println("Boom!")
+	}
+	check(scanner.Err())
 }

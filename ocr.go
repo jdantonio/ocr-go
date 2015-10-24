e8d2995eec3d75e7cffc0c235cd95ad068149137
Jerry D'Antonio <stumpjumper@gmail.com>
2015-10-24 10:20:53 -0400

    Tests for func to convert LCD to integer.

--- ocr.go
+++ ocr.go
@@ -24,15 +24,45 @@ import (
 //  [' ', '_', ' ', '|', '_', '|', '_', '_', '|'], # 9
 //]
 
-const AccountNumberLength = 30 // for the test file' the real file will have 27
+const LcdDigitLength = 9
+const AccountNumberLength = 30 // for the test file; the real file will have 27
 
-var lcd = [...]string{" _ | ||_|", "     |  |", " _  _||_ ", " _  _| _|", "   |_|  |", " _ |_  _|", " _ |_ |_|", " _   |  |", " _ |_||_|", " _ |_|__|"}
+var LCD = []string{
+	" _ | ||_|", // 0
+	"     |  |", // 1
+	" _  _||_ ", // 2
+	" _  _| _|", // 3
+	"   |_|  |", // 4
+	" _ |_  _|", // 5
+	" _ |_ |_|", // 6
+	" _   |  |", // 7
+	" _ |_||_|", // 8
+	" _ |_|__|", // 9
+}
 
 func check(e error) {
 	if e != nil {
 		fmt.Println(e)
 		panic(e)
 	}
+}
+
+func LcdToDigit(str string) (int, error) {
+	const invalid = -1
+
+	if len(str) < LcdDigitLength {
+		return invalid, errors.New("too short")
+	} else if len(str) > LcdDigitLength {
+		return invalid, errors.New("too long")
+	}
+
+	for digit, lcd := range LCD {
+		if str == lcd {
+			return digit, nil
+		}
+	}
+
+	return invalid, errors.New("invalid characters")
 }
 
 func main() {
--- ocr_test.go
+++ ocr_test.go
@@ -2,7 +2,31 @@ package main
 
 import "testing"
 
-func TestSetup(t *testing.T) {
-	t.Skip("I haven't implemented anything yet.")
-	//t.Error("I'm in a bad mood.")
+func TestLcdToDigitWithGoodValues(t *testing.T) {
+	for expected, lcd := range LCD {
+		actual, err := LcdToDigit(lcd)
+		if err != nil {
+			t.Errorf("%v should be valid but returned error %v", actual, err)
+		}
+		if actual != expected {
+			t.Errorf("%v does not equal expected value %v", actual, expected)
+		}
+	}
+}
+
+func TestLcdToDigitWithBadValues(t *testing.T) {
+	invalids := []string{
+		"",              // empty string
+		"/-/-/-/-/",     // invalid characters
+		" _ ",           // too short
+		" _ | ||_|    ", // too long
+		"_________",     // does not match
+	}
+
+	for _, lcd := range invalids {
+		actual, err := LcdToDigit(lcd)
+		if err == nil {
+			t.Errorf("%v should be invalid but no error was returned", actual)
+		}
+	}
 }

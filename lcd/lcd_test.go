dd1549e4f2704cee5c9913a15c43df531e3f515e
Jerry D'Antonio <stumpjumper@gmail.com>
2015-10-25 13:05:08 -0400

    Added tests for lcd.ScanNext

--- lcd/lcd_test.go
+++ lcd/lcd_test.go
@@ -2,6 +2,56 @@ package lcd
 
 import "testing"
 
+const mockScannerValue = 123
+
+type mockScanner struct {
+	TotalLines, CurrentLine int
+}
+
+func (scanner mockScanner) Text() string {
+	switch scanner.CurrentLine {
+	case 0:
+		return "    _  _ "
+	case 1:
+		return "  | _| _|"
+	case 2:
+		return "  ||_  _|"
+	default:
+		return ""
+	}
+}
+
+func (scanner *mockScanner) Scan() bool {
+	scanner.CurrentLine++
+	return scanner.CurrentLine < 5
+}
+
+func (scanner *mockScanner) ShortCircuit() {
+	scanner.CurrentLine = 2
+}
+
+func TestScanNextWithValidInput(t *testing.T) {
+	actual, err := ScanNext(new(mockScanner))
+	if err != nil {
+		t.Errorf("%v should be valid but returned error %v", actual, err)
+	}
+	if actual != mockScannerValue {
+		t.Errorf("expected %v but got %v", mockScannerValue, actual)
+	}
+}
+
+func TestScanNextWithInvalidInput(t *testing.T) {
+	scanner := new(mockScanner)
+	scanner.ShortCircuit()
+	actual, err := ScanNext(scanner)
+	if err == nil {
+		t.Error("should have returned an error but didn't")
+	}
+	if actual != Invalid {
+		t.Errorf("expected invalid but got %v", actual)
+	}
+}
+
 func TestDigitToIntegerWithGoodValues(t *testing.T) {
 	for expected, digit := range AllDigits {
 		actual, err := digit.Integer()
